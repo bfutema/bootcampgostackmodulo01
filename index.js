28c67faad1ace2093d6867a772b96de6ca392c93
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-11-15 18:20:44 -0300

    middlewares

--- index.js
+++ index.js
@@ -10,17 +10,44 @@ server.use(express.json());
 
 const users = ['Diego', 'Cláudio', 'Victor', 'Bruno'];
 
+server.use((req, res, next) => {
+  console.time('Request');
+  console.log(`Método: ${req.method}; URL: ${req.url}`);
+
+  next();
+
+  console.timeEnd('Request');
+});
+
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'User name is required' });
+  }
+
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+
+  if (!user) {
+    return res.status(400).json({ error: 'User does not exists' });
+  }
+
+  req.user = user;
+
+  return next();
+}
+
 server.get('/users', (req, res) => {
   return res.json(users);
 });
 
-server.get('/users/:index', (req, res) => {
-  const { index } = req.params;
-
-  return res.json(users[index]);
+server.get('/users/:index',checkUserInArray, (req, res) => {
+  return res.json(req.user);
 });
 
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExists, (req, res) => {
   const { name } = req.body;
 
   users.push(name);
@@ -28,7 +55,7 @@ server.post('/users', (req, res) => {
   return res.json(users);
 });
 
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUserExists, checkUserInArray, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -37,12 +64,12 @@ server.put('/users/:index', (req, res) => {
   return res.json(users);
 });
 
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index',checkUserInArray, (req, res) => {
   const { index } = req.params;
   
   users.splice(index, 1);
 
-  return res.send('Usuário excluído com sucesso!');
+  return res.json({ error: 'Usuário excluído com sucesso!' });
 })
 
 server.listen(3000);
